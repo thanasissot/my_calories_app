90558b930fba76e6cb6dddd84c9472f95d9e3b64
thanasissot <thanasissot1@gmail.com>
2022-08-07 14:16:39 +0300

    add fetching Food from DB

--- calories-rest/src/main/java/com/mycalories/caloriesrest/controller/FoodController.java
+++ calories-rest/src/main/java/com/mycalories/caloriesrest/controller/FoodController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/food")
+@RequestMapping("/foods")
 @RequiredArgsConstructor
 public class FoodController {
     private final FoodService foodService;
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FoodComponent } from "./food/food.component";
 
-
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'foods', component: FoodComponent}
+]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
